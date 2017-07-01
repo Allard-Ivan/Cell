97c3e6e08ed55ae20e2af098a6370ba61ac94ef9
Allard Ivan <allard1854@gmail.com>
2017-07-01 18:42:46 +0800

    Spring Boot Seed Project
    master branch used seed project, can not update again.

--- src/main/java/com/dragonball/cell/config/WebMvcConfig.java
+++ src/main/java/com/dragonball/cell/config/WebMvcConfig.java
@@ -1,17 +1,29 @@
 package com.dragonball.cell.config;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.dragonball.cell.core.Result;
+import com.dragonball.cell.core.ResultCode;
+import com.dragonball.cell.core.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,29 +34,61 @@ import java.util.List;
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedHeaders("*")
-                        .allowedMethods("*")
-                        .allowedOrigins("*");
-            }
-        };
+    private final Logger logger = LoggerFactory.getLogger(WebMvcConfigurer.class);
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*");
     }
 
     @Bean
     public HttpMessageConverter<?> fastJsonConverter() {
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteNullNumberAsZero);
         List<MediaType> fastMediaTypes = new ArrayList<>();
         fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
         converter.setSupportedMediaTypes(fastMediaTypes);
         converter.setFastJsonConfig(fastJsonConfig);
         return converter;
+    }
+
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+        exceptionResolvers.add((request, response, handler, e) -> {
+            Result result = new Result();
+            if (handler instanceof HandlerMethod) {
+                HandlerMethod handlerMethod = (HandlerMethod) handler;
+
+                if (e instanceof ServiceException) {//业务失败的异常，如“账号或密码错误”
+                    result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
+                    logger.info(e.getMessage());
+                } else {
+                    result.setCode(ResultCode.INTERNAL_SERVER_ERROR).setMessage("接口 [" + request.getRequestURI() + "] 内部错误，请联系管理员");
+                    String message = String.format("接口 [%s] 出现异常，方法：%s.%s，异常摘要：%s",
+                            request.getRequestURI(),
+                            handlerMethod.getBean().getClass().getName(),
+                            handlerMethod.getMethod().getName(),
+                            e.getMessage());
+                    logger.error(message, e);
+                }
+            } else {
+                if (e instanceof NoHandlerFoundException) {
+                    result.setCode(ResultCode.NOT_FOUND).setMessage("接口 [" + request.getRequestURI() + "] 不存在");
+                } else {
+                    result.setCode(ResultCode.INTERNAL_SERVER_ERROR).setMessage(e.getMessage());
+                    logger.error(e.getMessage(), e);
+                }
+            }
+            responseResult(response, result);
+            return new ModelAndView();
+        });
     }
 
     @Override
@@ -56,5 +100,16 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+
+    private void responseResult(HttpServletResponse response, Result result) {
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Content-type", "application/json;charset=UTF-8");
+        response.setStatus(200);
+        try {
+            response.getWriter().write(JSON.toJSONString(result));
+        } catch (IOException ex) {
+            logger.error(ex.getMessage());
+        }
     }
 }
--- src/main/java/com/dragonball/cell/core/Result.java
+++ src/main/java/com/dragonball/cell/core/Result.java
@@ -1,4 +1,4 @@
-package com.company.project.core;
+package com.dragonball.cell.core;
 
 import com.alibaba.fastjson.JSON;
 
